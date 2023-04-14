ecf309aa278afd13dcb3f2fbc41b125bcd624652
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-14 13:05:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const findMatching = (arr, str) =>{
+
+   return arr.filter(x => {
+
+        if (x.toUpperCase() === str.toUpperCase()){
+            return x
+        } else if(str !== str){
+            return []
+        }
+
+    })
+}
+
+const fuzzyMatch = (arr, str) =>{
+  
+    return arr.filter(x => {
+
+        if (x.toUpperCase()[0] === str.toUpperCase()[0]){
+            return x
+        } else if(str !== str){
+            return []
+        }
+
+    })
+}
+
+const  matchName = (arr, str) => {
+
+
+ return arr.filter(x => {
+        if(x.name === str){return [x]}
+    })
+
+}
